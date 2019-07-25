975671995fbe0d66e24b55dc078ded5da46dcc0f
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-07-25 20:56:50 +0600

    code style exercise

--- src/Domain/Entity/AbstractEntity.php
+++ src/Domain/Entity/AbstractEntity.php
@@ -1,16 +1,51 @@
 <?php
+/**
+ * Abstract Entity
+ * php version 7.3.7
+ * 
+ * @category Abstract
+ * @package  CleanPhp
+ * @author   Alexandr Krylov <alexandr.krylov@gmail.com>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.html AGPLv3
+ * @version  GIT: <0.0>
+ * @link     https://github.com/alexandr-krylov/CleanPHP.git
+ */
+
 namespace CleanPhp\Invoicer\Domain\Entity;
 
+/**
+ * Abstract Entity
+ * 
+ * @category Abstract
+ * @package  CleanPhp
+ * @author   Alexandr Krylov <alexandr.krylov@gmail.com>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.html AGPLv3
+ * @link     https://github.com/alexandr-krylov/CleanPHP.git
+ */
 abstract class AbstractEntity
 {
     protected $id;
 
-    public function getId()
+    /**
+     * Getter for $id
+     * 
+     * @return int
+     */
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function setId($id)
+    /**
+     * $id int
+     * Setter for $id
+     * 
+     * @name 
+     * @param $id int
+     * 
+     * @return $this
+     */
+    public function setId(int $id)
     {
         $this->id = $id;
         return $this;
